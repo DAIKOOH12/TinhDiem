c41670eb66444775adf87e2f9e1746397d57e36e
Truong <daikooh1234@gmail.com>
2024-06-17 04:45:37 +0700

    update giao dien

--- application/views/form_multiple.php
+++ application/views/form_multiple.php
@@ -5,14 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo base_url() . 'assets/css/Vupload.css'; ?>">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"> -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="<?php echo base_url() . 'assets/fontawesome/css/all.min.css'; ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Itim|Lobster|Montserrat:500|Noto+Serif|Nunito|Patrick+Hand|Roboto+Mono:100,100i,300,300i,400,400i,500,500i,700,700i|Roboto+Slab|Saira" rel="stylesheet">
     <title>Document</title>
 </head>
 
 <body>
+
     <div class="wrapper">
         <div class="header">
             <ul>
@@ -31,96 +33,114 @@
                         <p>3</p>
                     </div>
                 </li>
+                <li class="form_4_progessbar">
+                    <div>
+                        <p>4</p>
+                    </div>
+                </li>
             </ul>
         </div>
         <div class="form_wrap">
             <div class="form_1 data_info">
-                <h2>Nhập đáp án cho các đề</h2>
+                <h2>Nhập thông tin môn thi</h2>
                 <form action="<?php echo site_url('site/spreadsheet_import') ?>" method="post" enctype="multipart/form-data" id="form-upload-key">
                     <div class="form_container">
                         <div class="input_wrap">
-                            <label for="email">Nhập mã đề</label>
-                            <input type="text" name="made" class="input" id="made">
+                            <label for="email">Danh sách môn đã có</label>
+                            <select name="" id="dsmon">
+                                <option value="">Kinh tế - Chính trị (7E13000)</option>
+                                <option value="">Tiếng Anh cơ bản 3 (7E13203)</option>
+                                <option value="">Triết học Mác - Lenin (7E32910)</option>
+                                <option value="">Tiếng Anh cơ bản 1 (7E20304)</option>
+                            </select>
                         </div>
                         <div class="input_wrap">
                             <label for="email">Nhập mã môn</label>
-                            <input type="text" name="mamon" class="input" id="mamon">
+                            <input type="text" name="made" class="input" id="made">
                         </div>
                         <div class="input_wrap">
-                            <label class="mr-2">Nhập bộ đáp án</label>
-                            <input type="file" name="upload_file_key" id="upload_file" class="form-control" placeholder="Enter file">
-                        </div>
-                        <div class="input_wrap" style="display: flex;justify-content: center;">
-                            <button class="btn btn-primary" id="btn-upload-key"><i class="fa-solid fa-file-arrow-up" style="color: #ffffff;margin-right: 10px;"></i>Tải lên</button>
+                            <label for="email">Nhập tên môn</label>
+                            <input type="text" name="mamon" class="input" id="mamon">
                         </div>
                     </div>
                 </form>
             </div>
             <div class="form_2 data_info" style="display: none;">
-                <h2>Nhập file đáp án</h2>
-                <form action="<?php echo site_url("site/upload") ?>" method="post" enctype="multipart/form-data" id="form-upload">
+                <h2>Nhập đáp án</h2>
+                <form action="<?php echo site_url('site/spreadsheet_import') ?>" method="post" enctype="multipart/form-data" id="form-upload-key">
                     <div class="form_container">
                         <div class="input_wrap">
-                            <div class="form-control" data-trigger="fileinput" style="width: 100%;"><i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename">Định dạng file hỗ trợ: .xlsx</span></div>
+                            <label for="input-made">Nhập mã đề</label>
+                            <div class="add-files-group">
+                                <div class="input-group">
+                                    <input type="text" name="" id="" class="input input-made" placeholder="Định dạng file hỗ trợ .xlsx">
+                                    <label for="upload_file" id="lbinput-file"><i class="fa-solid fa-file-circle-plus fa-2xl" style="color: #f19b38;margin-left: 10px"></i></label>
+                                    <input type="file" name="upload_file_key" id="upload_file" class="form-control" placeholder="Enter file">
+                                </div>
+                            </div>
+                            <div class="btn-add-files">
+                                <i class="fa-solid fa-plus fa-xl" style="color: white;"></i>
+                                <span style="color:white;">Thêm mã đề</span>
+                            </div>
                         </div>
-                        <div class="input_wrap">
-                            <!-- <span>Chọn mã đề</span>
-                            <select name="" id="">
-                                <option value="">A113</option>
-                                <option value="">A114</option>
-                                <option value="">A115</option>
-                                <option value="">A116</option>
-                            </select> -->
+                    </div>
+                </form>
+            </div>
+            <div class="form_3 data_info" style="display: none;">
+                <h2>Nhập bộ câu trả lời</h2>
+                <form action="<?php echo site_url("site/upload") ?>" method="post" enctype="multipart/form-data" id="form-upload">
+                    <div class="form_container">
+
+                        <div class="input_wrap" style="display: flex;justify-content: center;">
+                            <label for="file" id="lb-filedapan"><i class="fa-solid fa-file-circle-plus fa-2xl" style="color: white;margin-right: 10px"></i>Chọn file</label>
                         </div>
-                        <div class="input_wrap">
-                            <span class="input-group-addon btn-outline-primary btn-file"><span class="fileinput-new"><i class="glyphicon glyphicon-paperclip"></i> Thêm các tệp tin</span><input type="file" name="file[]" multiple id="file" placeholder="Định dạng file hỗ trợ: .xlsx"></span>
+                        <div class="input_wrap" style="display: flex;justify-content: center;">
+                            <input type="checkbox" name="cbSenđEmail" id="cbSendEmail"><span>Nhận thông báo qua email</span>
+                            <input type="file" name="file[]" multiple id="file" placeholder="Định dạng file hỗ trợ: .xlsx" style="display: none;">
+                            <!-- <button class="btn btn-primary" id="upload-btn"><i class="fa-solid fa-file-arrow-up" style="color: #ffffff;margin-right: 10px;"></i>Tải lên</button> -->
                         </div>
-                        <div class="input_wrap"  style="display: flex;justify-content: center;">
-                        <button class="btn btn-primary" id="upload-btn"><i class="fa-solid fa-file-arrow-up" style="color: #ffffff;margin-right: 10px;"></i>Tải lên</button>
+                        <div class="input_wrap" style="display: flex;justify-content: center;">
+                            <h4>Các file đã thêm</h4>
                         </div>
-                        <div class="progress" style="display:none;width: 100%;">
+                        <!-- <div class="progress" style="display:none;width: 100%;">
                             <div id="progress-bar" class="progress-bar progress-bar-success progress-bar-striped " role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
                                 20%
                             </div>
-                        </div>
-                        <h4>Các file đã thêm</h4>
+                        </div> -->
                         <ul class="list-group">
 
                         </ul>
                     </div>
-
                 </form>
-
             </div>
-            <div class="form_3 data_info" style="display: none;">
+            <div class="form_4 data_info" style="display: none;">
                 <h2>Đang xử lí.....</h2>
-                <form action="<?php echo site_url("site/upload") ?>" id="form-upload-key">
-                    <div class="input_wrap" style="display: flex;justify-content: center;">
-                        <div class="loader"></div>
-                    </div>
-                </form>
+                <div class="input_wrap" style="display: flex;justify-content: center;">
+                    <div class="loader"></div>
+                </div>
             </div>
         </div>
         <div class="btns_wrap">
             <div class="common_btns form_1_btns">
-                <button type="button" class="btn_next">Next <span class="icon">
+                <button type="button" class="btn_next">Tiếp<span class="icon">
                         <ion-icon name="arrow-forward-sharp"></ion-icon>
                     </span></button>
             </div>
             <div class="common_btns form_2_btns" style="display: none;">
                 <button type="button" class="btn_back"><span class="icon">
                         <ion-icon name="arrow-back-sharp"></ion-icon>
-                    </span>Back</button>
-                <button type="button" class="btn_next">Next <span class="icon">
+                    </span>Quay lại</button>
+                <button type="button" class="btn_next">Lưu đáp án <span class="icon">
                         <ion-icon name="arrow-forward-sharp"></ion-icon>
                     </span></button>
             </div>
             <div class="common_btns form_3_btns" style="display: none;">
                 <button type="button" class="btn_back"><span class="icon">
                         <ion-icon name="arrow-back-sharp"></ion-icon>
-                    </span>Back</button>
-                <button type="button" class="btn_done">Done</button>
+                    </span>Quay lại</button>
+                <button type="button" class="btn_next">Thực hiện</button>
             </div>
+
         </div>
     </div>
 
--- assets/js/upload_form.js
+++ assets/js/upload_form.js
@@ -4,24 +4,32 @@ $(function () {
 	var progressBar = $('#progress-bar');
 	var inputFileKey = $('input[name=upload_file_key]');
 	var uploadURIKey = $('#form-upload-key').attr('action');
+	
 
 	var form_1 = document.querySelector(".form_1");
 	var form_2 = document.querySelector(".form_2");
 	var form_3 = document.querySelector(".form_3");
+	var form_4 = document.querySelector(".form_4");
+
 
 
 	var form_1_btns = document.querySelector(".form_1_btns");
 	var form_2_btns = document.querySelector(".form_2_btns");
 	var form_3_btns = document.querySelector(".form_3_btns");
+	var form_4_btns = document.querySelector(".form_4_btns");
+
 
 
 	var form_1_next_btn = document.querySelector(".form_1_btns .btn_next");
 	var form_2_back_btn = document.querySelector(".form_2_btns .btn_back");
 	var form_2_next_btn = document.querySelector(".form_2_btns .btn_next");
 	var form_3_back_btn = document.querySelector(".form_3_btns .btn_back");
+	var form_3_next_btn = document.querySelector(".form_3_btns .btn_next");
 
 	var form_2_progessbar = document.querySelector(".form_2_progessbar");
 	var form_3_progessbar = document.querySelector(".form_3_progessbar");
+	var form_4_progessbar = document.querySelector(".form_4_progessbar");
+
 
 	var btn_done = document.querySelector(".btn_done");
 	var btn_back = document.querySelector(".btn_back");
@@ -39,6 +47,7 @@ $(function () {
 	});
 
 	form_2_back_btn.addEventListener("click", function () {
+		console.log('back');
 		form_1.style.display = "block";
 		form_2.style.display = "none";
 
@@ -52,7 +61,7 @@ $(function () {
 		form_2.style.display = "none";
 		form_3.style.display = "block";
 
-		form_3_btns.style.display = "none";
+		form_3_btns.style.display = "flex";
 		form_2_btns.style.display = "none";
 
 
@@ -62,6 +71,7 @@ $(function () {
 	});
 
 	form_3_back_btn.addEventListener("click", function () {
+		// console.log('back');
 		form_2.style.display = "block";
 		form_3.style.display = "none";
 
@@ -70,13 +80,31 @@ $(function () {
 
 		form_3_progessbar.classList.remove("active");
 	});
+	form_3_next_btn.addEventListener("click", function () {
+		form_4.style.display = "block";
+		form_3.style.display = "none";
 
-	btn_done.addEventListener("click", function () {
-		modal_wrapper.classList.add("active");
-	})
+		form_3_btns.style.display = "none";
+		form_3_btns.style.display = "none";
 
-	shadow.addEventListener("click", function () {
-		modal_wrapper.classList.remove("active");
+		if($('#cbSendEmail').is(":checked")){
+			$(".form_4").html('<div class="input_wrap" style="display: flex;justify-content: center;width:120%;"><h2>Kết quả sẽ được gửi qua email!!!</h2></div>')
+		}
+		form_4_progessbar.classList.add("active");
+
+	});
+
+	// btn_done.addEventListener("click", function () {
+	// 	modal_wrapper.classList.add("active");
+	// })
+
+	// shadow.addEventListener("click", function () {
+	// 	modal_wrapper.classList.remove("active");
+	// })
+
+	$('.btn-add-files').on('click',function(e){
+		// console.log($('add-files-group'));
+		$('.add-files-group').append('<div class="input-group" style="display: flex;width:100%;align-items: center;"><input type="text" name="" id="" class="input input-made" placeholder="Định dạng file hỗ trợ .xlsx"><label for="upload_file" id="lbinput-file"><i class="fa-solid fa-file-circle-plus fa-2xl" style="color: #f19b38;margin-left: 10px"></i></label><input type="file" name="upload_file_key" id="upload_file" class="form-control" placeholder="Enter file"></div>');
 	})
 
 	$('#btn-upload-key').on('click', function (even) {
@@ -111,9 +139,54 @@ $(function () {
 		}
 	});
 
+	$('#file').on('change',function(event){
+		var filesToUpload = inputFile[0].files;
+		var filesToUpload = inputFile[0].files;
+		// event.preventDefault();
+		// make sure there is file(s) to upload
+		if (filesToUpload.length > 0) {
+			// provide the form data
+			// that would be sent to sever through ajax
+			var formData = new FormData();
+
+			for (var i = 0; i < filesToUpload.length; i++) {
+				var file = filesToUpload[i];
+
+				formData.append("file[]", file, file.name);
+			}
+
+			// now upload the file using $.ajax
+			$.ajax({
+				url: uploadURI,
+				type: 'post',
+				data: formData,
+				processData: false,
+				contentType: false,
+				success: function () {
+					listFilesOnServer();
+					// alert("Thành công");
+				},
+				xhr: function () {
+					var xhr = new XMLHttpRequest();
+					xhr.upload.addEventListener("progress", function (event) {
+						if (event.lengthComputable) {
+							var percentComplete = Math.round((event.loaded / event.total) * 100);
+							// console.log(percentComplete);
+
+							$('.progress').show();
+							progressBar.css({ width: percentComplete + "%" });
+							progressBar.text(percentComplete + '%');
+						};
+					}, false);
+
+					return xhr;
+				}
+			});
+		}
+	})
 	$('#upload-btn').on('click', function (event) {
 		var filesToUpload = inputFile[0].files;
-		event.preventDefault();
+		// event.preventDefault();
 		// make sure there is file(s) to upload
 		if (filesToUpload.length > 0) {
 			// provide the form data
