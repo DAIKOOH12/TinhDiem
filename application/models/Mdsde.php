45d2c41a083d42d978dc00cadeb736e5766cd9a9
Truong <daikooh1234@gmail.com>
2024-07-01 12:26:54 +0700

    cap nhat quan ly de

---
+++ application/models/Mdsde.php
@@ -0,0 +1,11 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+require FCPATH . 'vendor/autoload.php';
+
+class Mdsde extends CI_Model{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function getDSDe(){
+        
+    }
+}
